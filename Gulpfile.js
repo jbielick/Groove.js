0df40db3e51ae1d799c410c5b54143768978a289
Josh Bielick <josh@fragmentlabs.com>
2014-08-28 21:01:27 -0400

    build task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     mocha = require('gulp-spawn-mocha'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify');
 
 gulp.task('watch', ['connect'], function() {
   gulp.watch('{lib,test}/**/*.{coffee,js}', [
@@ -13,12 +14,12 @@ gulp.task('watch', ['connect'], function() {
 });
 
 gulp.task('compile', function() {
-  return gulp.src('./lib/**/*.coffee')
+  return gulp.src('./src/**/*.coffee')
       .pipe(coffee({bare: true}).on('error', function(err) {throw err}))
       .pipe(gulp.dest('./src'));
 });
 
-gulp.task('test', function() {
+gulp.task('test', ['compile'], function() {
   return gulp.src('./test/**/*.js', {read: false})
     .pipe(mocha({ui: 'tdd', bail: true}));
 });
@@ -33,5 +34,11 @@ gulp.task('reload', function() {
   gulp.src('test/*.html')
     .pipe(connect.reload())
 });
+
+gulp.task('build', ['test'], function() {
+  return gulp.src('lib/legato.js')
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('lib/legato.min.js'));
+})
 
 gulp.task('default', ['connect']);
